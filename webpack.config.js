e4b5ad4754e57f2096def105adfe56d8d0108386
Konstantin Burnaev <kbourn@gmail.com>
2017-07-16 21:29:22 +1000

    Site updated at: 2017-07-16 11:29:22 UTC

--- webpack.config.js
+++ webpack.config.js
@@ -14,8 +14,7 @@ module.exports = {
 
   entry: {
     "init": "init",
-    "using-refs-in-react": "using-refs-in-react",
-    "dealing-with-jarred-animation-in-react": "dealing-with-jarred-animation-in-react"
+    "using-refs-in-react": "using-refs-in-react"
   },
 
   externals: {},
